8ecf8119ce16cb732c98d48c8b0bb5f002904cfd
marialgoldsmith001 <marial.goldsmith@gmail.com>
2026-02-18 12:25:51 -0800

    fixed issues with mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import './App.css';
-import { Grid, Stack, Item, Container} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Stack, Container} from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import Title from './components/title';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
 
 function App() {
   return (
-    // <Container sx={{height: 350}}>
-      <Title/>
-      /* <Grid container spacing={2}>
+    <Container sx={{height: 350}}>
+      <Grid container spacing={2}>
         <Grid size={4}>
           <Stack spacing={2}>
             <Item>Column 1 - Row 1</Item>
@@ -18,8 +31,8 @@ function App() {
         <Grid size="auto">
           <Title/>
         </Grid>
-      </Grid> */
-    // </Container>
+      </Grid>
+    </Container>
   )
 }
 
