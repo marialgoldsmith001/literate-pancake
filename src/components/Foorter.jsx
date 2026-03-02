7c4638e0475b1757d3b0e72002bf8f242769607b
marialgoldsmith001 <marial.goldsmith@gmail.com>
2026-03-02 12:35:18 -0600

    deleting public json files and finishing the contact me drawer

--- src/components/Foorter.jsx
+++ src/components/Foorter.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 import MdPhone from '@mui/icons-material/Phone';
-import { Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
+import { Box, BottomNavigation, BottomNavigationAction, Drawer, Typography, Stack } from '@mui/material';
 
 function Footer() {
-  const [value, setValue] = React.useState(0);
+  const [infoState, setInfoState] = useState([]);
+  const [drawerState, setDrawerState] = useState(false);
+  
+  const toggleDrawer = (open) => {
+    setDrawerState(open);
+  }
 
-  const navigate = useNavigate();
+  useEffect(() =>{
+      fetch("aboutme.json").then(response => response.json()).then(data => setInfoState(data))
+  },[]);
+  
+  const contactMe = () => (
+    <Box sx={{ width: 800, position: 'fixed', bottom: 60, left: 0, right: 0, margin: 'auto', bgcolor: 'background.paper' }}
+      role="presentation"
+      onClick={() => toggleDrawer(false)}
+      onKeyDown={() => toggleDrawer(false)}
+    >
+      <Stack sx={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+        <Typography variant="h6" >This is my contact details:</Typography>
+        <Typography variant="body1" >{infoState.name}</Typography>
+        <Typography variant="body1">{infoState.phonenumber}</Typography>
+        <Typography variant="body1">{infoState.email}</Typography>
+        <Typography variant="body1">{infoState.github}</Typography>
+      </Stack>
+    </Box>
+  );
+
   return (
     <Box sx={{ width: 800, position: 'fixed', bottom: 0, left: 0, right: 0, margin: 'auto' }}>
         <BottomNavigation
         showLabels
-        value={value}
-        onChange={(event, newValue) => {
-            setValue(newValue);
-        }}
         >
-        <BottomNavigationAction label="Contact me" icon={<MdPhone />} onClick={() => navigate("/contactme")}/>
+        <BottomNavigationAction label="Contact me" icon={<MdPhone />} onClick={() => toggleDrawer(true)}/>
+        <Drawer open={drawerState} onClose={() => toggleDrawer(false)}>
+            {contactMe()}
+        </Drawer>
         </BottomNavigation>
     </Box>
     );
